de71415e4fddde851dae68f3b9f2ad502e8ecb61
Chris Davis <chris.davis5440@gmail.com>
2018-03-30 12:24:22 +0000

    Added scroll link in hero section and started work on featured section

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,4 +4,10 @@ $(function() {
   //   $(".navbar").css("background", "rgba(21, 34, 53, .95)");
   // });
   
+  //Smooth Scroll
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 800,
+    easing: 'easeInOutQuart'
+  });
+  
 });
