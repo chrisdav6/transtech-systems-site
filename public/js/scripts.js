9c0f1b3b294e77cc43c155e3afb101c706e1597b
Chris Davis <chris.davis5440@gmail.com>
2018-04-02 20:03:21 +0000

    added about us section below hero section on index page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,8 +4,13 @@ $(function() {
   //   $(".navbar").css("background", "rgba(21, 34, 53, .95)");
   // });
   
-  //Smooth Scroll on featured only
-  var scroll = new SmoothScroll('a[href="#featured"]', {
+  //Smooth Scroll on featured and about only
+  var scrollFeatured = new SmoothScroll('a[href="#featured"]', {
+    speed: 800,
+    easing: 'easeInOutQuart'
+  });
+  
+  var scrollAbout = new SmoothScroll('a[href="#about"]', {
     speed: 800,
     easing: 'easeInOutQuart'
   });
