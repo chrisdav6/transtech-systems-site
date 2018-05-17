d9c97fea7ab97d7acbc51f49229d80bb92346047
Chris Davis <chris.davis5440@gmail.com>
2018-05-17 14:50:10 +0000

    created contact corporate submit form logic, hiding button and showing spinner and message

--- public/js/formsHandling.js
+++ public/js/formsHandling.js
@@ -1,5 +1,44 @@
 $(function() {
   
+  //Corporate Contact Form
+  $("#corporateContactForm").on("submit", function(event) {
+    event.preventDefault();
+    
+    let $nameInput = $("#name");
+		let $emailInput = $("#email");
+		let $messageInput = $("#message");
+		let $address = $("#address"); //Honeypot field
+		let $corpContactBtn = $("#corpContactBtn");
+
+		if($address.val() !== "") {
+			alert("Nice try bot!");
+			return false;
+		}
+    
+		//Hide the submit button
+		$corpContactBtn.hide();
+		//Show the spinner gif
+		$(".ajaxSpinner").show();
+
+		//Wait 2 seconds
+		setTimeout(function() {
+			//Hide the spinner gif
+			$(".ajaxSpinner").hide();
+			//Show the submit button
+			$corpContactBtn.show();
+			//Flash the success message
+			$(".submitMessage").fadeIn("slow").delay(2000).fadeOut("fast");
+			//Clear form fields
+			$nameInput.val("");
+			$emailInput.val("");
+			$messageInput.val("");
+		},2000);
+		
+		//Send Ajax to PHP file
+  });
+  
+  
+  
   //Sales Request Form
   
   //Show Privacy statement if country is not US
