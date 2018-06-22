211070e0d197fb28fd138ec221998f916281b239
Chris Davis <chris.davis5440@gmail.com>
2018-06-22 19:19:58 +0000

    built product videos section

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -42,4 +42,23 @@ $(function() {
     },500);
   });
   
+  
+  
+  //Load Youtube Videos on Click
+  $(".youtubeEmbed").each(function() {
+    // Set the BG image from the youtube ID
+    $(this).css('background-image', 'url(//i.ytimg.com/vi/' + this.id + '/hqdefault.jpg)');
+    // Click the video div
+    $(document).delegate('#' + this.id, 'click', function() {
+      // Build embed URL
+      var iframe_url = "//www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+      // Grab extra parameters set on div
+      if ($(this).data('params')) iframe_url += '&' + $(this).data('params');
+      // Build iframe tag
+      var iframe = $('<iframe/>', {'allowfullscreen':'allowfullscreen', 'frameborder': '0', 'src': iframe_url})
+      // Replace the YouTube thumbnail with YouTube HTML5 Player
+      $(this).append(iframe);
+    });// /click
+  }); // /each video
+
 });
