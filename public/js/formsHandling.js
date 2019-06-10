5315bb403ebc679b2318fca883248e0c242dbd9b
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-06-10 08:42:46 -0400

    Testing honeypot on contactCorporate, hiding with javascript rather than css, also removed the option to tab down to field

--- public/js/formsHandling.js
+++ public/js/formsHandling.js
@@ -1,54 +1,59 @@
-$(function() {
-  
-  //When Available - Show Privacy statement on forms if country is not US
-  $(".privacy").hide();
-  $("#confirmPrivacy").prop("checked", true);
-  
-  $("#country").on("input", function() {
-    let countryVal = $("#country").val();
-    $("#confirmPrivacy").prop("checked", false);
-    
-    if(countryVal !== "United States") {
-      $(".privacy").slideDown();
-      $('#state option:contains("Not Available")').prop('selected',true);
-    } else {
-      $(".privacy").slideUp();
-      $("#confirmPrivacy").prop("checked", true);
-      $('#state option:contains("Alabama")').prop('selected',true);
-    }
-  });
-  
-  
-  
-  //Corporate Contact Form Submit
-  $("#corporateContactForm").on("submit", function(event) {
+$(function () {
 
+	//When Available - Show Privacy statement on forms if country is not US
+	$(".privacy").hide();
+	$("#confirmPrivacy").prop("checked", true);
+
+	$("#country").on("input", function () {
+		let countryVal = $("#country").val();
+		$("#confirmPrivacy").prop("checked", false);
+
+		if (countryVal !== "United States") {
+			$(".privacy").slideDown();
+			$('#state option:contains("Not Available")').prop('selected', true);
+		} else {
+			$(".privacy").slideUp();
+			$("#confirmPrivacy").prop("checked", true);
+			$('#state option:contains("Alabama")').prop('selected', true);
+		}
+	});
+
+
+
+	//Hide honeypot field
+	let $businessAddress = $(".business-address");
+	$businessAddress.hide();
+
+	//Corporate Contact Form Submit
+	$("#corporateContactForm").on("submit", function (event) {
+
+		//Select submit button
 		let $corpContactBtn = $("#corpContactBtn");
-    
+
 		//Hide the submit button
 		$corpContactBtn.hide();
 		//Show the spinner gif
 		$(".fa-spinner").show();
 
 		//Wait 2 seconds
-		setTimeout(function() {
+		setTimeout(function () {
 			//Hide the spinner gif
 			$(".fa-spinner").hide();
 			//Flash the success message
-			$(".submitMessage").fadeIn("slow").delay(2000).fadeOut("fast", function() {
+			$(".submitMessage").fadeIn("slow").delay(2000).fadeOut("fast", function () {
 				//Show the submit button
 				$corpContactBtn.show();
 			});
-		},2000);
-		
-  });
+		}, 2000);
+
+	});
 
 
-  //Sales Request form does not need spinner gif
-  
-  //Repair Request Form does not need spinner gif
+	//Sales Request form does not need spinner gif
 
-	
+	//Repair Request Form does not need spinner gif
+
+
 	//Product Repair Form remove required serial # if PTS3000 or Other is selected
 	$("#repairRequestForm").on("change", function (event) {
 
@@ -63,54 +68,54 @@ $(function() {
 		}
 
 	});
-  
-
-  //Product Registration Form does not need spinner gif
 
 
-  //Product Registration Form remove required serial # if PTS3000 is selected
-  $("#productRegistrationForm").on("change", function(event) {
+	//Product Registration Form does not need spinner gif
+
+
+	//Product Registration Form remove required serial # if PTS3000 is selected
+	$("#productRegistrationForm").on("change", function (event) {
 
 		let $serial = $("#serial");
 		let $pts3000 = $("#register3");
 
-		if($pts3000.is(':checked')) {
+		if ($pts3000.is(':checked')) {
 			$serial.prop("required", false);
 		} else {
 			$serial.prop("required", true);
 		}
-		
-  });
-  
-  
+
+	});
+
+
 	//Download Manuals Form Product Pages
-	$("#currentCustomerNo").on("change", function() {
+	$("#currentCustomerNo").on("change", function () {
 		$(".hearAbout").fadeIn();
 		$("#reach").attr("required", true);
 	});
-				
-	$("#currentCustomerYes").on("change", function() {
+
+	$("#currentCustomerYes").on("change", function () {
 		$(".hearAbout").fadeOut();
 		$("#reach").attr("required", false);
 	});
-	
+
 	//Get dataId from passed to modal
 	$('#manualDownload').on('show.bs.modal', function (e) {
-    let dataId = $(e.relatedTarget).attr('data-id');
-    console.log(dataId);
-    
+		let dataId = $(e.relatedTarget).attr('data-id');
+		console.log(dataId);
+
 		//Download Manuals Form Submit
-	  $("#manualForm").on("submit", function(event) {
-	    
-	    let $nameInput = $("#name");
-	    let $compnayInput = $("#company");
+		$("#manualForm").on("submit", function (event) {
+
+			let $nameInput = $("#name");
+			let $compnayInput = $("#company");
 			let $emailInput = $("#email");
 			let $manualName = $("#manualName");
 			let $manualDownloadBtn = $("#manualDownloadBtn");
-			
+
 			//Send ManualName on form when email is sent
 			$manualName.val(dataId);
-	    
+
 			//Hide the submit button
 			$manualDownloadBtn.hide();
 			//Show the spinner gif
@@ -118,9 +123,9 @@ $(function() {
 				"display": "block"
 			});
 			$(".fa-spinner").show();
-	
+
 			//Wait 2 seconds
-			setTimeout(function() {
+			setTimeout(function () {
 				//Hide the spinner gif
 				$(".fa-spinner").hide();
 				//Flash the success message
@@ -170,21 +175,21 @@ $(function() {
 				$nameInput.val("");
 				$compnayInput.val("");
 				$emailInput.val("");
-			},2000);
-	  });
+			}, 2000);
+		});
 	});
-	
-  
-  
-  //Close the modal when download link is clicked
-  $("#manualDownloadLink").on("click", function() {
-  	$(".submitMessage").hide();
-  	$("#manualDownloadLink").hide();
-  	$("#manualDownloadBtn").show();
-  	$('#manualDownload').modal('hide');
-  	//Redirect back to original window from modal
-  	let currentLocation = window.location.href;
-  	window.location.href = currentLocation;
-  });
-  
+
+
+
+	//Close the modal when download link is clicked
+	$("#manualDownloadLink").on("click", function () {
+		$(".submitMessage").hide();
+		$("#manualDownloadLink").hide();
+		$("#manualDownloadBtn").show();
+		$('#manualDownload').modal('hide');
+		//Redirect back to original window from modal
+		let currentLocation = window.location.href;
+		window.location.href = currentLocation;
+	});
+
 });
