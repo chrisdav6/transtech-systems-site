37db174a7aa7d126f06c011bc9a4f55d05bc98b1
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-02-07 09:05:25 -0500

    updated gulp to ver4, removed Napa and WOC from tradeshows page

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,36 @@
-var gulp = require("gulp");
-var browserSync = require("browser-sync").create();
-var sass = require("gulp-sass");
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
 //Complie Sass & Inject into browser
-gulp.task("sass", function() {
-  return gulp.src(["node_modules/bootstrap/scss/bootstrap.scss", "public/scss/*.scss"])
-  .pipe(sass({outputStyle: 'compressed'}))
-  .pipe(gulp.dest("public/css"))
-  .pipe(browserSync.stream());
+gulp.task('sass', function(done) {
+  return gulp
+    .src(['node_modules/bootstrap/scss/bootstrap.scss', 'public/scss/*.scss'])
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.stream());
+  done();
 });
 
 //Move CSS libraries to public folder
-gulp.task("css", function() {
-  return gulp.src("node_modules/lightbox2/dist/css/lightbox.min.css")
-  .pipe(gulp.dest("public/css"))
-  .pipe(browserSync.stream());
+gulp.task('css', function() {
+  return gulp
+    .src('node_modules/lightbox2/dist/css/lightbox.min.css')
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.stream());
 });
 
 //Move JS files to Public folder
-gulp.task("js", function() {
-  return gulp.src([
-    "node_modules/bootstrap/dist/js/bootstrap.min.js", 
-    "node_modules/jquery/dist/jquery.min.js", 
-    "node_modules/popper.js/dist/umd/popper.min.js",
-    "node_modules/lightbox2/dist/js/lightbox.min.js"])
-  .pipe(gulp.dest("public/js"))
-  .pipe(browserSync.stream());
+gulp.task('js', function() {
+  return gulp
+    .src([
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/popper.js/dist/umd/popper.min.js',
+      'node_modules/lightbox2/dist/js/lightbox.min.js'
+    ])
+    .pipe(gulp.dest('public/js'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task("default", ["sass", "css", "js"]);
+gulp.task('default', gulp.series([('sass', 'css', 'js')]));
