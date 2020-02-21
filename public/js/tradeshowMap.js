6636f79dee6a991ef43171541d08983a53a75715
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-02-21 08:52:24 -0500

    Removed RMACES and Mississippi from tradeshows page and map

--- public/js/tradeshowMap.js
+++ public/js/tradeshowMap.js
@@ -1,237 +1,239 @@
-// Asynchronously Load the map API 
+// Asynchronously Load the map API
 var script = document.createElement('script');
-script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyD4_58SHQlSV-JLIOT4y2yYWxM62U2kO38&callback=initMap";
+script.src =
+  'https://maps.googleapis.com/maps/api/js?key=AIzaSyD4_58SHQlSV-JLIOT4y2yYWxM62U2kO38&callback=initMap';
 document.body.appendChild(script);
 
-
 function initMap() {
-	var map;
+  var map;
 
-	var mapOptions = {
-		center: {
-			lat: 38.415,
-			lng: -93.30
-		},
-		zoom: 4,
-		mapTypeId: 'roadmap',
-		scrollwheel: false
-	};
+  var mapOptions = {
+    center: {
+      lat: 38.415,
+      lng: -93.3
+    },
+    zoom: 4,
+    mapTypeId: 'roadmap',
+    scrollwheel: false
+  };
 
-	// Display a map on the page
-	map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  // Display a map on the page
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-	// Map Marker Locations
-	var markers = [
-		//['CAPA, Pinehurst, NC', 35.189523, -79.462592],
-		//['APAI, Indianapolis, IN', 40.031171, -86.939831],
-		//['TRB, Washington D.C.', 38.911744, -77.018697],
-		['RMACES, Denver, CO', 39.771474, -104.806882],
-		['ConExpo, Las Vegas, Nevada', 36.161515, -115.161274],
-		// ['APAM, Battle Creek, MI', 42.296783, -85.076260],
-		['Ohio Asphalt Expo, Columbus, OH', 40.139683, -82.976837],
-		['MAPA, Bangor, ME', 44.831204, -68.781424],
-		['CalAPA, Anaheim, CA', 33.830204, -117.909703],
-		// ['Intermat ASEAN, Bangkok, Thailand', 13.588022, 100.580894],
-		['TXAPA, San Antonio, TX', 29.461083, -98.499359],
-		['NEAUPG, Albany, NY', 42.728662, -73.799678],
-		['SEAUPG, Knoxville, TN', 35.952985, -83.935225],
-		['World of Asphalt, Atlanta, GA', 33.764130, -84.420611],
-	];
+  // Map Marker Locations
+  var markers = [
+    //['CAPA, Pinehurst, NC', 35.189523, -79.462592],
+    //['APAI, Indianapolis, IN', 40.031171, -86.939831],
+    //['TRB, Washington D.C.', 38.911744, -77.018697],
+    //['RMACES, Denver, CO', 39.771474, -104.806882],
+    ['ConExpo, Las Vegas, Nevada', 36.161515, -115.161274],
+    // ['APAM, Battle Creek, MI', 42.296783, -85.076260],
+    ['Ohio Asphalt Expo, Columbus, OH', 40.139683, -82.976837],
+    ['MAPA, Bangor, ME', 44.831204, -68.781424],
+    ['CalAPA, Anaheim, CA', 33.830204, -117.909703],
+    // ['Intermat ASEAN, Bangkok, Thailand', 13.588022, 100.580894],
+    ['TXAPA, San Antonio, TX', 29.461083, -98.499359],
+    ['NEAUPG, Albany, NY', 42.728662, -73.799678],
+    ['SEAUPG, Knoxville, TN', 35.952985, -83.935225],
+    ['World of Asphalt, Atlanta, GA', 33.76413, -84.420611]
+  ];
 
-	// Info Window Content
-	var infoWindowContent = [
+  // Info Window Content
+  var infoWindowContent = [
+    // CAPA
+    // [
+    // 	'<div class="map-text">' +
+    // 	'<img class="d-block mb-3" width="100" src="../img/capa-logo.png">' +
+    // 	'<span>CAPA</span><br>' +
+    // 	'<hr>' +
+    // 	'Pinehurst, NC<br>' +
+    // 	'April 1-3, 2020<br>' +
+    // 	'<p><a class="link" href="http://www.carolinaasphalt.org/aws/CAPA/pt/sp/calendar" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+    // 	'</div>'
+    // ],
 
-		// CAPA
-		// [
-		// 	'<div class="map-text">' +
-		// 	'<img class="d-block mb-3" width="100" src="../img/capa-logo.png">' +
-		// 	'<span>CAPA</span><br>' +
-		// 	'<hr>' +
-		// 	'Pinehurst, NC<br>' +
-		// 	'April 1-3, 2020<br>' +
-		// 	'<p><a class="link" href="http://www.carolinaasphalt.org/aws/CAPA/pt/sp/calendar" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-		// 	'</div>'
-		// ],
+    // APAI
+    // [
+    // 	'<div class="map-text">' +
+    // 	'<img class="d-block mb-3" width="100" src="../img/apai-logo.png">' +
+    // 	'<span>APAI</span><br>' +
+    // 	'<hr>' +
+    // 	'Indianapolis, IN<br>' +
+    // 	'December 11-13, 2019<br>' +
+    // 	'<p><a class="link" href="https://asphaltindiana.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+    // 	'</div>'
+    // ],
 
-		// APAI
-		// [
-		// 	'<div class="map-text">' +
-		// 	'<img class="d-block mb-3" width="100" src="../img/apai-logo.png">' +
-		// 	'<span>APAI</span><br>' +
-		// 	'<hr>' +
-		// 	'Indianapolis, IN<br>' +
-		// 	'December 11-13, 2019<br>' +
-		// 	'<p><a class="link" href="https://asphaltindiana.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-		// 	'</div>'
-		// ],
+    // TRB
+    // [
+    // 	'<div class="map-text">' +
+    // 	'<img class="d-block mb-3" width="100" src="../img/trb-logo.png">' +
+    // 	'<span>TRB - Booth #1046</span><br>' +
+    // 	'<hr>' +
+    // 	'Washington D.C.<br>' +
+    // 	'January 12-16, 2020<br>' +
+    // 	'<p><a class="link" href="http://www.trb.org/AnnualMeeting/AnnualMeeting.aspx" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+    // 	'</div>'
+    // ],
 
-		// TRB
-		// [
-		// 	'<div class="map-text">' +
-		// 	'<img class="d-block mb-3" width="100" src="../img/trb-logo.png">' +
-		// 	'<span>TRB - Booth #1046</span><br>' +
-		// 	'<hr>' +
-		// 	'Washington D.C.<br>' +
-		// 	'January 12-16, 2020<br>' +
-		// 	'<p><a class="link" href="http://www.trb.org/AnnualMeeting/AnnualMeeting.aspx" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-		// 	'</div>'
-		// ],
+    // RMACES
+    // [
+    // 	'<div class="map-text">' +
+    // 	'<img class="d-block mb-3" width="100" src="../img/rmaces-logo.png">' +
+    // 	'<span>RMACES</span><br>' +
+    // 	'<hr>' +
+    // 	'Denver, CO<br>' +
+    // 	'February 19-20, 2020<br>' +
+    // 	'<p><a class="link" href="https://www.rmaces.org/index.php" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+    // 	'</div>'
+    // ],
 
-		// RMACES
-		[
-			'<div class="map-text">' +
-			'<img class="d-block mb-3" width="100" src="../img/rmaces-logo.png">' +
-			'<span>RMACES</span><br>' +
-			'<hr>' +
-			'Denver, CO<br>' +
-			'February 19-20, 2020<br>' +
-			'<p><a class="link" href="https://www.rmaces.org/index.php" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-			'</div>'
-		],
+    // ConExpo
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/conexpo-logo.png">' +
+        '<span>ConExpo - Booth #C30947</span><br>' +
+        '<hr>' +
+        'Las Vegas, NV<br>' +
+        'March 10-14, 2020<br>' +
+        '<p><a class="link" href="http://www.conexpoconagg.com" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>'
+    ],
 
-		// ConExpo
-		[
-			'<div class="map-text">' +
-			'<img class="d-block mb-3" width="100" src="../img/conexpo-logo.png">' +
-			'<span>ConExpo - Booth #C30947</span><br>' +
-			'<hr>' +
-			'Las Vegas, NV<br>' +
-			'March 10-14, 2020<br>' +
-			'<p><a class="link" href="http://www.conexpoconagg.com" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-			'</div>'
-		],
+    //APAM
+    // [
+    // 	'<div class="map-text">' +
+    // 	'<img class="d-block mb-3" width="100" src="../img/apam-logo.png">' +
+    // 	'<span>APAM</span><br>' +
+    // 	'<hr>' +
+    // 	'Battle Creek, MI<br>' +
+    // 	'March 18-19, 2020<br>' +
+    // 	'<p><a class="link" href="http://www.apa-mi.org/index.php" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+    // 	'</div>'
+    // ],
 
-		//APAM
-		// [
-		// 	'<div class="map-text">' +
-		// 	'<img class="d-block mb-3" width="100" src="../img/apam-logo.png">' +
-		// 	'<span>APAM</span><br>' +
-		// 	'<hr>' +
-		// 	'Battle Creek, MI<br>' +
-		// 	'March 18-19, 2020<br>' +
-		// 	'<p><a class="link" href="http://www.apa-mi.org/index.php" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-		// 	'</div>'
-		// ],
+    //Ohio
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/ohio-logo.png">' +
+        '<span>Ohio Asphalt Expo</span><br>' +
+        '<hr>' +
+        'Columbus, OH<br>' +
+        'March 24-25, 2020<br>' +
+        '<p><a class="link" href="http://www.flexiblepavements.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>'
+    ],
 
-		//Ohio
-		[
-			'<div class="map-text">' +
-			'<img class="d-block mb-3" width="100" src="../img/ohio-logo.png">' +
-			'<span>Ohio Asphalt Expo</span><br>' +
-			'<hr>' +
-			'Columbus, OH<br>' +
-			'March 24-25, 2020<br>' +
-			'<p><a class="link" href="http://www.flexiblepavements.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-			'</div>'
-		],
+    // Maine
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/maine-logo.png">' +
+        '<span>MAPA</span><br>' +
+        '<hr>' +
+        'Bangor, ME<br>' +
+        'April 7-8, 2020<br>' +
+        '<p><a class="link" href="https://maine-apa.org/paving-seminar" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>'
+    ],
 
-		// Maine
-		[
-			'<div class="map-text">' +
-			'<img class="d-block mb-3" width="100" src="../img/maine-logo.png">' +
-			'<span>MAPA</span><br>' +
-			'<hr>' +
-			'Bangor, ME<br>' +
-			'April 7-8, 2020<br>' +
-			'<p><a class="link" href="https://maine-apa.org/paving-seminar" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-			'</div>'
-		],
+    //CalAPA
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/calapa-logo.png">' +
+        '<span>CalAPA Spring Conference</span><br>' +
+        '<hr>' +
+        'Anaheim, CA<br>' +
+        'April 16-17, 2020<br>' +
+        '<p><a class="link" href="http://www.calapa.net" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>'
+    ],
 
-		//CalAPA
-		[
-			'<div class="map-text">' +
-			'<img class="d-block mb-3" width="100" src="../img/calapa-logo.png">' +
-			'<span>CalAPA Spring Conference</span><br>' +
-			'<hr>' +
-			'Anaheim, CA<br>' +
-			'April 16-17, 2020<br>' +
-			'<p><a class="link" href="http://www.calapa.net" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-			'</div>'
-		],
+    //Intermat ASEAN
+    // [
+    // 	'<div class="map-text">' +
+    // 	'<img class="d-block mb-3" width="100" src="../img/intermat-asean-logo.png">' +
+    // 	'<span>Intermat ASEAN</span><br>' +
+    // 	'<hr>' +
+    // 	'Bangkok, Thailand<br>' +
+    // 	'September 9-11, 2020<br>' +
+    // 	'<p><a class="link" href="https://asean.intermatconstruction.com" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+    // 	'</div>'
+    // ],
 
-		//Intermat ASEAN
-		// [
-		// 	'<div class="map-text">' +
-		// 	'<img class="d-block mb-3" width="100" src="../img/intermat-asean-logo.png">' +
-		// 	'<span>Intermat ASEAN</span><br>' +
-		// 	'<hr>' +
-		// 	'Bangkok, Thailand<br>' +
-		// 	'September 9-11, 2020<br>' +
-		// 	'<p><a class="link" href="https://asean.intermatconstruction.com" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-		// 	'</div>'
-		// ],
+    //TXAPA
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/txapa-logo.png">' +
+        '<span>TXAPA</span><br>' +
+        '<hr>' +
+        'San Antonio, TX<br>' +
+        'September 15-18, 2020<br>' +
+        '<p><a class="link" href="https://www.texasasphalt.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>'
+    ],
 
-		//TXAPA
-		[
-			'<div class="map-text">' +
-			'<img class="d-block mb-3" width="100" src="../img/txapa-logo.png">' +
-			'<span>TXAPA</span><br>' +
-			'<hr>' +
-			'San Antonio, TX<br>' +
-			'September 15-18, 2020<br>' +
-			'<p><a class="link" href="https://www.texasasphalt.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-			'</div>'
-		],
+    // NEAUPG
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/neaupg-logo.png">' +
+        '<span>NEAUPG</span><br>' +
+        '<hr>' +
+        'Albany, NY<br>' +
+        'October 28-29, 2020<br>' +
+        '<p><a class="link" href="http://www.neaupg.uconn.edu/meetingsevents" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>'
+    ],
 
-		// NEAUPG
-		[
-			'<div class="map-text">' +
-			'<img class="d-block mb-3" width="100" src="../img/neaupg-logo.png">' +
-			'<span>NEAUPG</span><br>' +
-			'<hr>' +
-			'Albany, NY<br>' +
-			'October 28-29, 2020<br>' +
-			'<p><a class="link" href="http://www.neaupg.uconn.edu/meetingsevents" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-			'</div>'
-		],
+    // SEAUPG
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/seaupg-logo.png">' +
+        '<span>SEAUPG</span><br>' +
+        '<hr>' +
+        'Knoxville, TN<br>' +
+        'November 16-19, 2020<br>' +
+        '<p><a class="link" href="http://www.seaupg.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>'
+    ],
 
-		// SEAUPG
-		[
-			'<div class="map-text">' +
-			'<img class="d-block mb-3" width="100" src="../img/seaupg-logo.png">' +
-			'<span>SEAUPG</span><br>' +
-			'<hr>' +
-			'Knoxville, TN<br>' +
-			'November 16-19, 2020<br>' +
-			'<p><a class="link" href="http://www.seaupg.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-			'</div>'
-		],
+    // World of Asphalt
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/woa-logo.png">' +
+        '<span>World of Asphalt 2021</span><br>' +
+        '<hr>' +
+        'Atlanta, GA<br>' +
+        'March 9-11, 2021<br>' +
+        '<p><a class="link" href="http://www.worldofasphalt.com" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>'
+    ]
+  ];
 
-		// World of Asphalt
-		[
-			'<div class="map-text">' +
-			'<img class="d-block mb-3" width="100" src="../img/woa-logo.png">' +
-			'<span>World of Asphalt 2021</span><br>' +
-			'<hr>' +
-			'Atlanta, GA<br>' +
-			'March 9-11, 2021<br>' +
-			'<p><a class="link" href="http://www.worldofasphalt.com" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-			'</div>'
-		],
+  // Display multiple markers on a map
+  var infoWindow = new google.maps.InfoWindow(),
+    marker,
+    i;
 
-	];
+  // Loop through our array of markers & place each one on the map
+  for (var i = 0; i < markers.length; i++) {
+    var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
 
+    marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: markers[i][0]
+    });
 
-	// Display multiple markers on a map
-	var infoWindow = new google.maps.InfoWindow(), marker, i;
-
-	// Loop through our array of markers & place each one on the map  
-	for (var i = 0; i < markers.length; i++) {
-		var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-
-		marker = new google.maps.Marker({
-			position: position,
-			map: map,
-			title: markers[i][0]
-		});
-
-		// Allow each marker to have an info window    
-		google.maps.event.addListener(marker, 'click', (function (marker, i) {
-			return function () {
-				infoWindow.setContent(infoWindowContent[i][0]);
-				infoWindow.open(map, marker);
-			}
-		})(marker, i));
-	}
-
+    // Allow each marker to have an info window
+    google.maps.event.addListener(
+      marker,
+      'click',
+      (function(marker, i) {
+        return function() {
+          infoWindow.setContent(infoWindowContent[i][0]);
+          infoWindow.open(map, marker);
+        };
+      })(marker, i)
+    );
+  }
 }
