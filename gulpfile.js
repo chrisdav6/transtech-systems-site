0b944819af30814d61d56dedace88818aabea542
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-04-05 14:34:31 -0400

    Updated to gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,58 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
 //Complie Sass & Inject into browser
-gulp.task('sass', function() {
-  return gulp
+// gulp.task('sass', function() {
+//   return gulp
+//     .src(['node_modules/bootstrap/scss/bootstrap.scss', 'public/scss/*.scss'])
+//     .pipe(sass({ outputStyle: 'compressed' }))
+//     .pipe(gulp.dest('public/css'))
+//     .pipe(browserSync.stream());
+// });
+
+// //Move CSS libraries to public folder
+// gulp.task('css', function() {
+//   return gulp
+//     .src('node_modules/lightbox2/dist/css/lightbox.min.css')
+//     .pipe(gulp.dest('public/css'))
+//     .pipe(browserSync.stream());
+// });
+
+// //Move JS files to Public folder
+// gulp.task('js', function() {
+//   return gulp
+//     .src([
+//       'node_modules/bootstrap/dist/js/bootstrap.min.js',
+//       'node_modules/jquery/dist/jquery.min.js',
+//       'node_modules/popper.js/dist/umd/popper.min.js',
+//       'node_modules/lightbox2/dist/js/lightbox.min.js'
+//     ])
+//     .pipe(gulp.dest('public/js'))
+//     .pipe(browserSync.stream());
+// });
+
+// gulp.task('default', ['sass', 'css', 'js']);
+
+function sass(cb) {
+  gulp
     .src(['node_modules/bootstrap/scss/bootstrap.scss', 'public/scss/*.scss'])
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(gulp.dest('public/css'))
     .pipe(browserSync.stream());
-});
+  cb();
+}
 
 //Move CSS libraries to public folder
-gulp.task('css', function() {
-  return gulp
+function css(cb) {
+  gulp
     .src('node_modules/lightbox2/dist/css/lightbox.min.css')
     .pipe(gulp.dest('public/css'))
     .pipe(browserSync.stream());
-});
+  cb();
+}
 
 //Move JS files to Public folder
-gulp.task('js', function() {
-  return gulp
+function js(cb) {
+  gulp
     .src([
       'node_modules/bootstrap/dist/js/bootstrap.min.js',
       'node_modules/jquery/dist/jquery.min.js',
@@ -30,6 +63,7 @@ gulp.task('js', function() {
     ])
     .pipe(gulp.dest('public/js'))
     .pipe(browserSync.stream());
-});
+  cb();
+}
 
-gulp.task('default', ['sass', 'css', 'js']);
+gulp.task('default', gulp.series(sass, css, js));
