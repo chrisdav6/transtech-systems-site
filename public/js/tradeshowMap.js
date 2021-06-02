e13f6d6dd015e0d9b47db023500583f6ad570500
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-06-02 11:07:21 -0400

    Added TXAPA 46th annual event

--- public/js/tradeshowMap.js
+++ public/js/tradeshowMap.js
@@ -34,7 +34,7 @@ function initMap() {
     // ['RMACES, Denver, CO', 39.771474, -104.806882],
     ['World of Asphalt, Nashville, TN', 36.101958, -86.375711],
     ['Ohio Asphalt Expo, Columbus, OH', 40.139683, -82.976837],
-    // ['TXAPA, San Antonio, TX', 29.461083, -98.499359],
+    ['TXAPA, San Antonio, TX', 29.461083, -98.499359],
     ['National Pavement Expo, Charlotte, NC', 35.216826, -80.851489],
     ['NEAUPG, Albany, NY', 42.728662, -73.799678],
     ['ConExpo, Las Vegas, Nevada', 36.161515, -115.161274]
@@ -187,16 +187,16 @@ function initMap() {
     ],
 
     //TXAPA
-    // [
-    //   '<div class="map-text">' +
-    //     '<img class="d-block mb-3" width="100" src="../img/txapa-logo.png">' +
-    //     '<span>TXAPA</span><br>' +
-    //     '<hr>' +
-    //     'San Antonio, TX<br>' +
-    //     'September 21-24, 2021<br>' +
-    //     '<p><a class="link" href="https://www.texasasphalt.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-    //     '</div>'
-    // ],
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/txapa-logo.png">' +
+        '<span>TXAPA</span><br>' +
+        '<hr>' +
+        'San Antonio, TX<br>' +
+        'September 21-24, 2021<br>' +
+        '<p><a class="link" href="https://www.texasasphalt.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>'
+    ],
 
     // Pavement Expo
     [
