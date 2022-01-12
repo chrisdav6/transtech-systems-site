715b98d5d9401a3934e4d5a465049307491e4136
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-01-12 15:49:28 -0500

    Removed TRB and Missouri from tradeshows page and map

--- public/js/tradeshowMap.js
+++ public/js/tradeshowMap.js
@@ -28,8 +28,8 @@ function initMap() {
     // ['Intermat ASEAN, Bangkok, Thailand', 13.588022, 100.580894],
     // ['SEAUPG, Knoxville, TN', 35.952985, -83.935225],
     // ['APAI, Indianapolis, IN', 40.031171, -86.939831],
-    ['TRB, Washington D.C.', 38.911744, -77.018697],
-    ['MAPA, Columbia, MO', 38.948857, -92.334012],
+    // ['TRB, Washington D.C.', 38.911744, -77.018697],
+    // ['MAPA, Columbia, MO', 38.948857, -92.334012],
     ['RMACES, Denver, CO', 39.771474, -104.806882],
     ['APAM, Mt Pleasant, MI', 43.595835, -84.772652],
     ['IAPA Annual Conference, Springfield, IL', 39.786973, -89.652644],
@@ -124,28 +124,28 @@ function initMap() {
     // ],
 
     // TRB
-    [
-      '<div class="map-text">' +
-        '<img class="d-block mb-3" width="100" src="../img/trb-logo.png">' +
-        '<span>TRB 101st Annual Meeting - Booth #854</span><br>' +
-        '<hr>' +
-        'Washington D.C.<br>' +
-        'January 9-13, 2022<br>' +
-        '<p><a class="link" href="http://www.trb.org/AnnualMeeting/AnnualMeeting.aspx" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-        '</div>',
-    ],
+    // [
+    //   '<div class="map-text">' +
+    //     '<img class="d-block mb-3" width="100" src="../img/trb-logo.png">' +
+    //     '<span>TRB 101st Annual Meeting - Booth #854</span><br>' +
+    //     '<hr>' +
+    //     'Washington D.C.<br>' +
+    //     'January 9-13, 2022<br>' +
+    //     '<p><a class="link" href="http://www.trb.org/AnnualMeeting/AnnualMeeting.aspx" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+    //     '</div>',
+    // ],
 
     // MAPA (Missouri)
-    [
-      '<div class="map-text">' +
-        '<img class="d-block mb-3" width="100" src="../img/mapa-logo.png">' +
-        '<span>32nd Annual MAPA Conference</span><br>' +
-        '<hr>' +
-        'Columbia, MO<br>' +
-        'January 10-12, 2022<br>' +
-        '<p><a class="link" href="https://moasphalt.org/events/annual-conference" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-        '</div>',
-    ],
+    // [
+    //   '<div class="map-text">' +
+    //     '<img class="d-block mb-3" width="100" src="../img/mapa-logo.png">' +
+    //     '<span>32nd Annual MAPA Conference</span><br>' +
+    //     '<hr>' +
+    //     'Columbia, MO<br>' +
+    //     'January 10-12, 2022<br>' +
+    //     '<p><a class="link" href="https://moasphalt.org/events/annual-conference" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+    //     '</div>',
+    // ],
 
     // RMACES
     [
