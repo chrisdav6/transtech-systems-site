ac473a51b00f482ae097f20679a1f5002e3c74fa
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-02-15 08:27:56 -0500

    Added CalAPA pring to tradeshows page and map

--- public/js/tradeshowMap.js
+++ public/js/tradeshowMap.js
@@ -25,7 +25,7 @@ function initMap() {
     //['CAPA, Pinehurst, NC', 35.189523, -79.462592],
     // ['MAPA, Bangor, ME', 44.831204, -68.781424],
     // ['CalAPA Fall, Ontario, CA', 34.038544, -117.60651],
-    // ['CalAPA Spring, Ontario, CA', 34.038544, -117.60651],
+    ['CalAPA Spring, Ontario, CA', 34.038544, -117.60651],
     // ['Intermat ASEAN, Bangkok, Thailand', 13.588022, 100.580894],
     // ['SEAUPG, Knoxville, TN', 35.952985, -83.935225],
     // ['APAI, Indianapolis, IN', 40.031171, -86.939831],
@@ -89,16 +89,16 @@ function initMap() {
     // ],
 
     //CalAPA Spring
-    // [
-    //   '<div class="map-text">' +
-    //     '<img class="d-block mb-3" width="100" src="../img/calapa-logo.png">' +
-    //     '<span>CalAPA Spring Conference</span><br>' +
-    //     '<hr>' +
-    //     'Ontario, CA<br>' +
-    //     'March 16-17, 2021<br>' +
-    //     '<p><a class="link" href="https://www.calapa.net/conference.html" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-    //     '</div>',
-    // ],
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/calapa-logo.png">' +
+        '<span>CalAPA Spring Conference</span><br>' +
+        '<hr>' +
+        'Ontario, CA<br>' +
+        'March 16-17, 2022<br>' +
+        '<p><a class="link" href="https://www.calapa.net/conference.html" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>',
+    ],
 
     //Intermat ASEAN
     // [
