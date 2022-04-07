212996d3af28bfba17928966e3a4bc2b4f5437c0
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-04-07 09:11:28 -0400

    Removed CalAPA spring from tradeshows page and map

--- public/js/tradeshowMap.js
+++ public/js/tradeshowMap.js
@@ -25,7 +25,7 @@ function initMap() {
     //['CAPA, Pinehurst, NC', 35.189523, -79.462592],
     // ['MAPA, Bangor, ME', 44.831204, -68.781424],
     // ['CalAPA Fall, Ontario, CA', 34.038544, -117.60651],
-    ['CalAPA Spring, Brea, CA', 33.928047, -117.883404],
+    // ['CalAPA Spring, Brea, CA', 33.928047, -117.883404],
     // ['Intermat ASEAN, Bangkok, Thailand', 13.588022, 100.580894],
     // ['SEAUPG, Knoxville, TN', 35.952985, -83.935225],
     ['APAI, Indianapolis, IN', 40.031171, -86.939831],
@@ -90,16 +90,16 @@ function initMap() {
     // ],
 
     //CalAPA Spring
-    [
-      '<div class="map-text">' +
-        '<img class="d-block mb-3" width="100" src="../img/calapa-logo.png">' +
-        '<span>CalAPA Spring Conference - Tech Tuneup</span><br>' +
-        '<hr>' +
-        'Brea, CA<br>' +
-        'April 5-6, 2022<br>' +
-        '<p><a class="link" href="https://calapa.weblinkconnect.com/events/CalAPASpring%20Conference%20Tech%20Tuneup%20%20Orange%20County-5025/details" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-        '</div>',
-    ],
+    // [
+    //   '<div class="map-text">' +
+    //     '<img class="d-block mb-3" width="100" src="../img/calapa-logo.png">' +
+    //     '<span>CalAPA Spring Conference - Tech Tuneup</span><br>' +
+    //     '<hr>' +
+    //     'Brea, CA<br>' +
+    //     'April 5-6, 2022<br>' +
+    //     '<p><a class="link" href="https://calapa.weblinkconnect.com/events/CalAPASpring%20Conference%20Tech%20Tuneup%20%20Orange%20County-5025/details" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+    //     '</div>',
+    // ],
 
     //Intermat ASEAN
     // [
