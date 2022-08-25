8ed6d5be3bda2f5cdac51c2747ad24e40f58d0bb
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-08-25 08:53:03 -0400

    Added RMAUPG annual conference to tradeshows and map

--- public/js/tradeshowMap.js
+++ public/js/tradeshowMap.js
@@ -41,6 +41,7 @@ function initMap() {
     ['World of Asphalt, Nashville, TN', 36.101958, -86.375711],
     // ['Ohio Asphalt Expo, Columbus, OH', 40.139683, -82.976837],
     ['TXAPA, San Antonio, TX', 29.461083, -98.499359],
+    ['RMAUPG 30th Annual Conference, Reno, NV', 39.545669, -119.806902],
     ['AASHTO, Orlando, FL', 28.551161, -81.373221],
     // ['TXAPA Quarterly Membership Meeting, Buda, TX', 30.068213, -97.830459],
     // ['APAI Golf Outing, Angola, IN', 41.630486, -85.01596],
@@ -286,6 +287,18 @@ function initMap() {
         'San Antonio, TX<br>' +
         'September 20-23, 2022<br>' +
         '<p><a class="link" href="https://www.texasasphalt.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>',
+    ],
+
+    //Rocky Mountain Asphalt
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/colorado-asphalt-logo.png">' +
+        '<span>RMAUPG 30th Annual Conference</span><br>' +
+        '<hr>' +
+        'Reno, NV<br>' +
+        'October 12-13, 2022<br>' +
+        '<p><a class="link" href="https://www.co-asphalt.com" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
         '</div>',
     ],
 
