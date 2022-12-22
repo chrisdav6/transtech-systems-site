3d49f7ede747ae28ca169a379540b93276865487
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-12-22 08:56:36 -0500

    Added Paiky Winter training to tradeshows page and map

--- public/js/tradeshowMap.js
+++ public/js/tradeshowMap.js
@@ -47,6 +47,7 @@ function initMap() {
     // ['APAI Golf Outing, Angola, IN', 41.630486, -85.01596],
     ['NAPA Midyear Meeting, Kansas City, MO', 39.127084, -94.522848],
     ['NAPA Annual Meeting, Miami Beach, FL', 25.793237, -80.133619],
+    ['PAIKY 2023 Winter Training, Louisville, KY', 38.203105, -85.631109],
     ['National Pavement Expo, Charlotte, NC', 35.216826, -80.851489],
     ['TXAPA MAPS Conference, Waco, TX', 31.554423, -97.162525],
     // ['NEAUPG, Albany, NY', 42.728662, -73.799678],
@@ -360,6 +361,18 @@ function initMap() {
         'Miami Beach, FL<br>' +
         'February 5-8, 2023<br>' +
         '<p><a class="link" href="https://www.asphaltpavement.org/programs/napa-events/napa-annual-meeting" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>',
+    ],
+
+    //PAIKY Winter Training
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/paiky-logo.png">' +
+        '<span>PAIKY 2023 Winter Training</span><br>' +
+        '<hr>' +
+        'Louisville, KY<br>' +
+        'February 15-17, 2023<br>' +
+        '<p><a class="link" href="https://members.paiky.org/site_event_detail.cfm?pk_association_event=25352" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
         '</div>',
     ],
 
