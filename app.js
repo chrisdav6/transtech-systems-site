087c19ae30f30f0d66695edd2b925ca89d664a9a
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-02-14 14:38:00 -0500

    Configuring URL path parameter from MGR redirect

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.locals.date = new Date().getFullYear();
 
 //MGR Domain Redirect
 app.get('/products/customerPortal', function (req, res) {
-  const path = req.params.path;
+  const path = req.query.path;
   res.render('products/customerPortal', {
     title: 'TransTech Systems Customer Portal',
     metaTitle: 'TransTech Systems Customer Portal',
