98a1ba355fbbe5d206de57b50264db01608ff888
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-06-30 13:49:27 -0400

    Added NPW booth number to tradeshows page and map

--- public/js/tradeshowMap.js
+++ public/js/tradeshowMap.js
@@ -435,7 +435,7 @@ function initMap() {
     [
       '<div class="map-text">' +
         '<img class="d-block mb-3" width="100" src="../img/pavement-expo-logo.png">' +
-        '<span>National Pavement Expo</span><br>' +
+        '<span>National Pavement Expo  - Booth #524</span><br>' +
         '<hr>' +
         'Tampa, FL<br>' +
         'January 23-25, 2024<br>' +
