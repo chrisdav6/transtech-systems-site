91f8c505deb0e2087abf214ad83e1abc0f269530
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-10-02 07:50:20 -0400

    Updated US distributors - removed Marcus

--- public/js/usMap.js
+++ public/js/usMap.js
@@ -39,7 +39,7 @@ function initMap() {
     ['TransTech Systems, Arkansas', 34.841498, -92.285615, 'transtech'],
     ['TransTech Systems, California', 36.692245, -119.620438, 'direct'],
     ['TransTech Systems, Colorado', 39.158062, -105.509759, 'transtech'],
-    ['TransTech Systems, Connecticut', 41.654445, -73.083801, 'direct'],
+    ['TransTech Systems, Connecticut', 41.654445, -73.083801, 'transtech'],
     ['GT Mid Atlantic, Inc., Delaware', 39.180106, -75.499366],
     ['Atlantic Supply, Jacksonville Florida', 30.31466, -81.655884],
     ['Atlantic Supply, Largo Florida', 27.909467, -82.787324],
@@ -54,9 +54,9 @@ function initMap() {
     ['TransTech Systems, Kansas', 38.570758, -98.446819, 'transtech'],
     ['TransTech Systems, Kentucky', 37.630124, -84.538175, 'direct'],
     ['Atlantic Supply, Louisiana', 30.489397, -91.903111],
-    ['TransTech Systems, Maine', 45.413876, -69.466553, 'direct'],
+    ['TransTech Systems, Maine', 45.413876, -69.466553, 'transtech'],
     ['GT Mid Atlantic, Inc., Maryland', 39.140858, -76.682258],
-    ['TransTech Systems, Massachusetts', 42.5207, -71.383667, 'direct'],
+    ['TransTech Systems, Massachusetts', 42.5207, -71.383667, 'transtech'],
     ['TransTech Systems, Michigan', 43.563136, -84.678763, 'direct'],
     ['TransTech Systems, Minnesota', 46.555991, -94.274308, 'direct'],
     ['Atlantic Supply, Mississippi', 32.509762, -89.428711],
@@ -64,7 +64,7 @@ function initMap() {
     ['TransTech Systems, Montana', 47.257664, -109.540552, 'transtech'],
     ['TransTech Systems, Nebraska', 41.648757, -99.658929, 'transtech'],
     ['TransTech Systems, Nevada', 40.079192, -116.892773, 'transtech'],
-    ['TransTech Systems, New Hampshire', 43.405047, -71.575928, 'direct'],
+    ['TransTech Systems, New Hampshire', 43.405047, -71.575928, 'transtech'],
     ['GT Mid Atlantic, Inc., New Jersey', 39.751595, -74.46653],
     ['TransTech Systems, New Mexico', 34.490538, -105.928939, 'direct'],
     ['TransTech Systems, New York', 42.776429, -73.866104, 'transtech'],
@@ -75,13 +75,13 @@ function initMap() {
     ['TransTech Systems, Oregon', 44.036589, -120.375932, 'transtech'],
     ['Groff Tractor & Equipment , Pennsylvania', 41.302571, -77.211914],
     ['Atlantic Supply, Puerto Rico', 18.245026, -66.569655],
-    ['TransTech Systems , Rhode Island', 41.631867, -71.471558, 'direct'],
+    ['TransTech Systems , Rhode Island', 41.631867, -71.471558, 'transtech'],
     ['TransTech Systems, South Carolina', 33.897999, -80.905078],
     ['TransTech Systems, South Dakota', 44.740993, -100.336661, 'transtech'],
     ['QC Lab Supply, Tennessee', 35.78074, -86.495376],
     ['TransTech Systems, Texas', 31.319622, -99.088073, 'direct'],
     ['TransTech Systems, Utah', 39.393259, -111.63642, 'transtech'],
-    ['TransTech Systems, Vermont', 43.890007, -72.737469, 'direct'],
+    ['TransTech Systems, Vermont', 43.890007, -72.737469, 'transtech'],
     ['TransTech Systems, Virginia', 37.598711, -78.339174],
     ['TransTech Systems, Washington', 47.426994, -119.815286, 'transtech'],
     ['TransTech Systems, West Virginia', 38.70075, -80.846758, 'transtech'],
@@ -161,8 +161,7 @@ function initMap() {
         '<span>TransTech Systems</span><br>' +
         '<hr>' +
         '1-800-724-6306<br>' +
-        'Attn: Marcus McCorison<br>' +
-        '<p><a class="link" href="mailto:mmccorison@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
+        '<p><a class="link" href="mailto:sales@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
         '</div>',
     ],
 
@@ -335,8 +334,7 @@ function initMap() {
         '<span>TransTech Systems</span><br>' +
         '<hr>' +
         '1-800-724-6306<br>' +
-        'Attn: Marcus McCorison<br>' +
-        '<p><a class="link" href="mailto:mmccorison@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
+        '<p><a class="link" href="mailto:sales@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
         '</div>',
     ],
 
@@ -363,8 +361,7 @@ function initMap() {
         '<span>TransTech Systems</span><br>' +
         '<hr>' +
         '1-800-724-6306<br>' +
-        'Attn: Marcus McCorison<br>' +
-        '<p><a class="link" href="mailto:mmccorison@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
+        '<p><a class="link" href="mailto:sales@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
         '</div>',
     ],
 
@@ -449,8 +446,7 @@ function initMap() {
         '<span>TransTech Systems</span><br>' +
         '<hr>' +
         '1-800-724-6306<br>' +
-        'Attn: Marcus McCorison<br>' +
-        '<p><a class="link" href="mailto:mmccorison@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
+        '<p><a class="link" href="mailto:sales@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
         '</div>',
     ],
 
@@ -575,8 +571,7 @@ function initMap() {
         '<span>TransTech Systems</span><br>' +
         '<hr>' +
         '1-800-724-6306<br>' +
-        'Attn: Marcus McCorison<br>' +
-        '<p><a class="link" href="mailto:mmccorison@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
+        '<p><a class="link" href="mailto:sales@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
         '</div>',
     ],
 
@@ -641,8 +636,7 @@ function initMap() {
         '<span>TransTech Systems</span><br>' +
         '<hr>' +
         '1-800-724-6306<br>' +
-        'Attn: Marcus McCorison<br>' +
-        '<p><a class="link" href="mailto:mmccorison@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
+        '<p><a class="link" href="mailto:sales@transtechsys.com?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
         '</div>',
     ],
 
