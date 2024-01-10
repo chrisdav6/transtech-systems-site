895478c0a064d8b85603f5fbf485d2ab6dd5e128
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-01-10 07:35:13 -0500

    Added WAPAs 65th conference to tradeshows and map

--- public/js/tradeshowMap.js
+++ public/js/tradeshowMap.js
@@ -61,7 +61,7 @@ function initMap() {
     ['TXAPA MAPS Conference, Waco, TX', 31.554423, -97.162525],
     ['TML Annual Conference, Dallas, TX', 33.083334, -96.809783],
     // ['NEAUPG, Albany, NY', 42.728662, -73.799678],
-    // ['WAPA, Wisconsin Dells, WI', 43.625082, -89.775456],
+    ['WAPA, Wisconsin Dells, WI', 43.625082, -89.775456],
     // ['Minnesota Asphalt Conference, Prior Lake, MN', 44.725722, -93.464836],
     // ['Missouri S&T Asphalt Conference, Rolla, MO', 37.947304, -91.768538],
     // [
@@ -519,16 +519,16 @@ function initMap() {
     // ],
 
     // WAPA
-    // [
-    //   '<div class="map-text">' +
-    //     '<img class="d-block mb-3" width="100" src="../img/wisconsin-asphalt-logo.png">' +
-    //     '<span>WAPA 64th Annual Conference</span><br>' +
-    //     '<hr>' +
-    //     'Wisconsin Dells, WI<br>' +
-    //     'November 28-29, 2023<br>' +
-    //     '<p><a class="link" href="https://www.wispave.org/event/wapas-64th-annual-conference-and-business-meeting" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-    //     '</div>',
-    // ],
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/wisconsin-asphalt-logo.png">' +
+        '<span>WAPA 65th Annual Conference</span><br>' +
+        '<hr>' +
+        'Wisconsin Dells, WI<br>' +
+        'December 3-4, 2024<br>' +
+        '<p><a class="link" href="https://www.wispave.org/event/wapas-65th-annual-conference-and-business-meeting" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>',
+    ],
 
     // Minnesota Asphalt Conference
     // [
