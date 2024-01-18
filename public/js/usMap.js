08911590630d0d42001efddd383b20008fab792a
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-01-18 10:18:32 -0500

    Added South Carolina associations to state and map

--- public/js/usMap.js
+++ public/js/usMap.js
@@ -592,6 +592,7 @@ function initMap() {
         'Attn: Mike Gordon<br>' +
         '<p><a class="link" href="mailto:mikeg@gtssi.net?subject=Inquiry from TransTech Systems Website"><i class="fas fa-envelope"></i> Email</a></p>' +
         '<p class="mt-0"><a class="link" href="https://www.gtssi.net" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '<p class="mt-0"><a class="link" href="http://seaupg.org" target="_blank"><i class="fa-solid fa-award fa-lg"></i> Visit SEAUPG</a></p>' +
         '</div>',
     ],
 
