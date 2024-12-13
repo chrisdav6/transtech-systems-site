6658162c2f1ed8544fe341adad1bf131d8b46817
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-12-13 11:42:44 -0500

    Added Paiky Winter to tradeshows and map

--- public/js/tradeshowMap.js
+++ public/js/tradeshowMap.js
@@ -57,7 +57,7 @@ function initMap() {
     // ['NAPA Annual Meeting, Maui, HI', 20.788874, -156.315323],
     ['Pave/X, Chattanooga, TN', 35.038162, -85.305639],
     // ['Greater Iowa Asphalt Conference, Des Moines, IA', 41.590298, -93.625914],
-    // ['PAIKY 2024 Winter Training, Louisville, KY', 38.203105, -85.631109],
+    ['PAIKY 2025 Winter Training, Louisville, KY', 38.203105, -85.631109],
     // ['National Pavement Expo, Tampa, FL', 27.940479, -82.455074],
     // ['TXAPA MAPS Conference, Waco, TX', 31.554423, -97.162525],
     // ['TML Annual Conference, Fort Worth, TX', 32.736147, -97.327084],
@@ -472,16 +472,16 @@ function initMap() {
     // ],
 
     //PAIKY Winter Training
-    // [
-    //   '<div class="map-text">' +
-    //     '<img class="d-block mb-3" width="100" src="../img/paiky-logo.png">' +
-    //     '<span>PAIKY 2024 Winter Training School</span><br>' +
-    //     '<hr>' +
-    //     'Louisville, KY<br>' +
-    //     'March 6-8, 2024<br>' +
-    //     '<p><a class="link" href="https://www.paiky.org" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
-    //     '</div>',
-    // ],
+    [
+      '<div class="map-text">' +
+        '<img class="d-block mb-3" width="100" src="../img/paiky-logo.png">' +
+        '<span>PAIKY 2025 Winter Training School</span><br>' +
+        '<hr>' +
+        'Louisville, KY<br>' +
+        'March 5-7, 2025<br>' +
+        '<p><a class="link" href="https://members.paiky.org/site_event_detail.cfm?pk_association_event=31956" target="_blank"><i class="fab fa-chrome"></i> Visit Website</a></p>' +
+        '</div>',
+    ],
 
     // Pavement Expo
     // [
