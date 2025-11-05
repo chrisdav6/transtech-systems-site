4bb4591e199415118514d5d1370eb2c19c169685
Chris Davis <chris.davis5440@gmail.com>
2025-11-05 14:06:06 -0500

    Restored list of receivers of the salesRequest form

--- app.js
+++ app.js
@@ -588,8 +588,7 @@ app.post('/products/salesRequest', function (req, res) {
       // setup email data with unicode symbols
       let mailOptions = {
         from: 'webforms@transtechsys.com', // sender address
-        // to: 'sales@transtechsys.com,tapkarian@transtechsys.com', // list of receivers
-        to: 'cdavis@transtechsys.com',
+        to: 'sales@transtechsys.com,tapkarian@transtechsys.com', // list of receivers
         replyTo: email,
         subject: 'TransTech Systems Sales Request Form', // Subject line
         text: message, // plain text body
